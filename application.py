0cb7ea550c9dd59bdfb2f3e9bcfcf5869eadc37f
Kacper <adamczyk911119@gmail.com>
2022-11-08 22:30:46 +0000

    Created initial flask app with very basic login and 'requirements.txt'

--- application.py
+++ application.py
@@ -1,1 +1,30 @@
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
 
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        name = request.form.get("name")
+        session["name"] = name #dictionary. global variable, can be accessed anywhere. created for each individual user
+
+        return redirect("/")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session["name"] = None
+    return redirect("/")
+
+if __name__ == '__main__':
+    app.run(debug=True)
