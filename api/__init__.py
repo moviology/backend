43fab2972221135ef2dc7cf6520ef701fc3de2ad
Kacper <adamczyk911119@gmail.com>
2022-11-13 15:00:38 +0000

    Successfully tested fetching and displaying all data from all tables. Also tested fetching a user's reviews and their datasets, starting with just the user id

--- api/__init__.py
+++ api/__init__.py
@@ -6,7 +6,7 @@ from bson.json_util import dumps
 import json
 import certifi
 
-with open("config/.secrets.json") as config_file:
+with open("../config/.secrets.json") as config_file:
     config = json.load(config_file)
 
 app = Flask(__name__)
@@ -23,31 +23,80 @@ uri = "mongodb+srv://%s:%s@%s.asjwyhf.mongodb.net/?retryWrites=true&w=majority" 
 )
 
 client = MongoClient(uri, tlsCAFile=certifi.where())
-
 # client object for a MongoDB instance called client, which allows you to connect and interact with your MongoDB server
 # When you instantiate the MongoClient(), you pass it the host of your MongoDB server, which is mongodb+srv...... in our case
 
-db = client.Moviology  # creating database on cluster
-# You then use the client instance to create a MongoDB database called moviology_db and save a reference to it in a variable called db.
+db = client.Moviology  # You then use the client instance to create a MongoDB database called moviology_db and save a
+# reference to it in a variable called db.
 
-bio_data = db.DATASETS  # Creating new collection for database
-# Then you create a collection called bioData on the flask_db database using the db variable.
-# Collections store a group of documents in MongoDB, like tables in relational databases.
+bio_data = db.DATASETS  # Creating new collection on the database using the db variable.
+movies_data = db.MOVIES  # Collections store a group of documents in MongoDB, like tables in relational databases.
+reviews_data = db.REVIEWS
+users_data = db.USERS
 
+# CREATING TEST DATA
 testData = {
+    "dataset_id": 3,
     "timestamp": [3, 6, 9, 12, 15],
     "heart_rate": [100, 120, 109, 112],
     "sweat": [0, 5, 6, 12],
     "machine_id": 128219,
 }
 
-# print(bioData.find_one({ "machine_id": 842 }))
+# FETCHING ALL DATA FROM ALL 4 TABLES
+all_bio_data = bio_data.find()
+all_movies_data = movies_data.find()
+all_reviews_data = reviews_data.find()
+all_users_data = users_data.find()
+
+# INSERTING TEST DATA INTO 'DATASETS' DATABASE
+bio_data.insert_one(testData)
+
+# ITERATING THROUGH EACH ELEMENT IN all_bio_data
+# for bioentry in all_bio_data:
+#     #bioentry is dictionary
+#     print(bioentry['machine_id'])
+#     print(bioentry.items())
+
+# PRINTING DATA FROM ALL TABLES IN JSON FORMAT
+print("Biometric data" + dumps(all_bio_data))
+print("Movie data" + dumps(all_movies_data))
+print("Review data" + dumps(all_reviews_data))
+print("User data" + dumps(all_users_data))
+
+print("\nFinding many entries that share common value for variable")
+for entry in bio_data.find({"machine_id": 128219}):  # finding many documents
+    print(entry)
+
+print("\nCounting entries")
+print(bio_data.count_documents({"machine_id": 128219}))
+
+# GETTING REVIEWS FOR A SINGLE MOVIE DIRECETOR USER
+testUser = users_data.find_one({"name": "kacper"})
+testUserId = testUser['user_id']
+print(f"\nUser name: {testUser['name']}")
+testReviews = reviews_data.find_one({"user_id": testUserId})
+print(f"\nReview description: {testReviews['description']}")
+testMovieId = testReviews['movie_id']
+testMovie = movies_data.find_one({"movie_id": testMovieId})
+print(f"\nMovie name: {testMovie['name']}")
+testDatasetId = testReviews['dataset_id']
+print(f"\nBiometric data")
+testBioData = bio_data.find({"dataset_id": testDatasetId})
+
+# ITERATING THROUGH ALL THE BIOMETRIC DATA THAT CORRESPOND TO THE SELECTED REVIEW, USING 'dataset_id' AS FOREIGN KEY
+for entry in testBioData:  # finding many documents
+    print(entry)
+
+
+
+
+
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    all_bio_data = bio_data.find()
-    print('all_bio_data', dumps(all_bio_data))
+    print(bio_data.find_one({"machine_id": 842}))
     # To display all the saved bioData, you use the find() method outside the code
     # responsible for handling POST requests, which returns all the biometric data available in the 'DATASETS' collection
 
@@ -77,4 +126,4 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
