addd5e2497e3212f3d93ba4624dbf3afc04ace28
Kacper <adamczyk911119@gmail.com>
2022-11-13 17:16:30 +0000

    Implemented 'login' and 'register' routes with password hashing. Commented out test code

--- api/__init__.py
+++ api/__init__.py
@@ -5,6 +5,7 @@ from urllib.parse import quote_plus
 from bson.json_util import dumps
 import json
 import certifi
+from passlib.hash import sha256_crypt
 
 with open("../config/.secrets.json") as config_file:
     config = json.load(config_file)
@@ -23,25 +24,13 @@ uri = "mongodb+srv://%s:%s@%s.asjwyhf.mongodb.net/?retryWrites=true&w=majority" 
 )
 
 client = MongoClient(uri, tlsCAFile=certifi.where())
-# client object for a MongoDB instance called client, which allows you to connect and interact with your MongoDB server
-# When you instantiate the MongoClient(), you pass it the host of your MongoDB server, which is mongodb+srv...... in our case
+db = client.Moviology
 
-db = client.Moviology  # You then use the client instance to create a MongoDB database called moviology_db and save a
-# reference to it in a variable called db.
-
-bio_data = db.DATASETS  # Creating new collection on the database using the db variable.
-movies_data = db.MOVIES  # Collections store a group of documents in MongoDB, like tables in relational databases.
+# CREATING COLLECTIONS ON THE DATABASE USING DB VARIABLE
+bio_data = db.DATASETS
+movies_data = db.MOVIES
 reviews_data = db.REVIEWS
 users_data = db.USERS
-
-# CREATING TEST DATA
-testData = {
-    "dataset_id": 3,
-    "timestamp": [3, 6, 9, 12, 15],
-    "heart_rate": [100, 120, 109, 112],
-    "sweat": [0, 5, 6, 12],
-    "machine_id": 128219,
-}
 
 # FETCHING ALL DATA FROM ALL 4 TABLES
 all_bio_data = bio_data.find()
@@ -49,8 +38,18 @@ all_movies_data = movies_data.find()
 all_reviews_data = reviews_data.find()
 all_users_data = users_data.find()
 
+
+# # CREATING TEST DATA
+# testData = {
+#     "dataset_id": 3,
+#     "timestamp": [3, 6, 9, 12, 15],
+#     "heart_rate": [100, 120, 109, 112],
+#     "sweat": [0, 5, 6, 12],
+#     "machine_id": 128219,
+# }
+
 # INSERTING TEST DATA INTO 'DATASETS' DATABASE
-bio_data.insert_one(testData)
+# bio_data.insert_one(testData)
 
 # ITERATING THROUGH EACH ELEMENT IN all_bio_data
 # for bioentry in all_bio_data:
@@ -59,63 +58,83 @@ bio_data.insert_one(testData)
 #     print(bioentry.items())
 
 # PRINTING DATA FROM ALL TABLES IN JSON FORMAT
-print("Biometric data" + dumps(all_bio_data))
-print("Movie data" + dumps(all_movies_data))
-print("Review data" + dumps(all_reviews_data))
-print("User data" + dumps(all_users_data))
-
-print("\nFinding many entries that share common value for variable")
-for entry in bio_data.find({"machine_id": 128219}):  # finding many documents
-    print(entry)
-
-print("\nCounting entries")
-print(bio_data.count_documents({"machine_id": 128219}))
-
-# GETTING REVIEWS FOR A SINGLE MOVIE DIRECETOR USER
-testUser = users_data.find_one({"name": "kacper"})
-testUserId = testUser['user_id']
-print(f"\nUser name: {testUser['name']}")
-testReviews = reviews_data.find_one({"user_id": testUserId})
-print(f"\nReview description: {testReviews['description']}")
-testMovieId = testReviews['movie_id']
-testMovie = movies_data.find_one({"movie_id": testMovieId})
-print(f"\nMovie name: {testMovie['name']}")
-testDatasetId = testReviews['dataset_id']
-print(f"\nBiometric data")
-testBioData = bio_data.find({"dataset_id": testDatasetId})
-
-# ITERATING THROUGH ALL THE BIOMETRIC DATA THAT CORRESPOND TO THE SELECTED REVIEW, USING 'dataset_id' AS FOREIGN KEY
-for entry in testBioData:  # finding many documents
-    print(entry)
-
-
-
-
-
-
+# print("Biometric data" + dumps(all_bio_data))
+# print("Movie data" + dumps(all_movies_data))
+# print("Review data" + dumps(all_reviews_data))
+# print("User data" + dumps(all_users_data))
+#
+# print("\nFinding many entries that share common value for variable")
+# for entry in bio_data.find({"machine_id": 128219}):  # finding many documents
+#     print(entry)
+#
+# print("\nCounting entries")
+# print(bio_data.count_documents({"machine_id": 128219}))
+#
+# # GETTING REVIEWS FOR A SINGLE MOVIE DIRECETOR USER
+# testUser = users_data.find_one({"name": "kacper"})
+# testUserId = testUser['user_id']
+# print(f"\nUser name: {testUser['name']}")
+# testReviews = reviews_data.find_one({"user_id": testUserId})
+# print(f"\nReview description: {testReviews['description']}")
+# testMovieId = testReviews['movie_id']
+# testMovie = movies_data.find_one({"movie_id": testMovieId})
+# print(f"\nMovie name: {testMovie['name']}")
+# testDatasetId = testReviews['dataset_id']
+# print(f"\nBiometric data")
+# testBioData = bio_data.find({"dataset_id": testDatasetId})
+#
+# # ITERATING THROUGH ALL THE BIOMETRIC DATA THAT CORRESPOND TO THE SELECTED REVIEW, USING 'dataset_id' AS FOREIGN KEY
+# for entry in testBioData:  # finding many documents
+#     print(entry)
+#
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    print(bio_data.find_one({"machine_id": 842}))
-    # To display all the saved bioData, you use the find() method outside the code
-    # responsible for handling POST requests, which returns all the biometric data available in the 'DATASETS' collection
-
-    # You save the biometric data you get from the database in a variable called allBioData, and then you edit the
-    # render_template() function call to pass the biometric data to the index.html template, which will be available in the
-    # template in a variable called bioDatas.
-
+    if not session.get("name"):
+        return redirect("/login")
     return {"server_data": dumps(all_bio_data)}
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        user_name = request.form.get("name")
+        user_email = request.form.get("email")
+        user_pw = request.form.get("password")
+
+        if users_data.find_one({"email": user_email}) is not None:
+            return render_template("registerFail.html")
+        else:
+            encrypted_password = sha256_crypt.hash(user_pw)
+            new_user = {
+                "name": user_name,
+                "email": user_email,
+                "password": encrypted_password
+            }
+            users_data.insert_one(new_user)
+
+            fetched_user = users_data.find_one({"email": user_email})
+            session["name"] = fetched_user["_id"]
+
+            return render_template("registerSuccess.html")
+    return render_template("register.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        name = request.form.get("name")
-        session[
-            "name"
-        ] = name  # dictionary. global variable, can be accessed anywhere. created for each individual user
+        user_email = request.form.get("email")
+        user_pw = request.form.get("password")
+        fetched_user = users_data.find_one({"email": user_email})
 
-        return redirect("/")
+        if fetched_user is None or not sha256_crypt.verify(user_pw, fetched_user['password']):
+            print("Incorrect login details")
+            return render_template("loginFail.html")
+        else:
+            print("Logged in successfully")
+            session["name"] = fetched_user["_id"]
+
+        return render_template("loginSuccess.html")
     return render_template("login.html")
 
 
